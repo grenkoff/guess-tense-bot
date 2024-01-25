d6cbbfd9d9a2aff7ed055ecffb2b2269122b54cb
Alexander Grenkov <a.grenkov@gmail.com>
2024-01-25 22:41:43 +0600

    add common commands for menu

--- bot/common/bot_cmds_list.py
+++ bot/common/bot_cmds_list.py
@@ -3,7 +3,7 @@ from aiogram.types import BotCommand
 
 private = [
     BotCommand(command='start', description='Welcome message'),
-    BotCommand(command='help', description='Keyboard`s buttons'),
+    BotCommand(command='help', description='Legends'),
     BotCommand(command='guess', description='Start new game'),
     BotCommand(command='author', description='Credits'),
 ]
