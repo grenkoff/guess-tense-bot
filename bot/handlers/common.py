b0acb26ad6f36292d3b3aeb787607f0cb9f5490e
Alexander Grenkov <a.grenkov@gmail.com>
2024-01-25 22:43:40 +0600

    replace bot.send_message instead message.answer

--- bot/handlers/common.py
+++ bot/handlers/common.py
@@ -1,13 +1,13 @@
 from random import choice
 
-from aiogram import F, Router
+from aiogram import F, Router, Bot
 from aiogram.filters import CommandStart, Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state
 from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.methods.delete_message import DeleteMessage
 
-from bot_instance import bot
+# from bot_instance import bot
 from utils.states import GuessRandom
 from data.data_fetcher import get_random
 from keyboards.reply import tenses_kb,finish_next_kb
@@ -34,9 +34,10 @@ available_tenses = {
 
 
 @router.message(StateFilter(None), CommandStart())
-async def cmd_start_no_state(message: Message, state: FSMContext):
+async def cmd_start_no_state(message: Message, state: FSMContext, bot: Bot):
     await message.delete()
-    await message.answer(
+    await bot.send_message(
+        chat_id=message.from_user.id,
         text=f"Hi, <b>{message.from_user.first_name}</b>! 🤝\n{WELCOME}",
         reply_markup=ReplyKeyboardRemove()
     )
@@ -53,18 +54,20 @@ async def cmd_start_finish_or_next_state(message: Message, state: FSMContext):
 
 
 @router.message(Command("help"))
-async def cmd_help(message: Message, state: FSMContext):
+async def cmd_help(message: Message, state: FSMContext, bot: Bot):
     await message.delete()
-    await message.answer(
+    await bot.send_message(
+        chat_id=message.from_user.id,
         text=f"{HELP}"
     )
 
 
 @router.message(Command("author"))
-async def cmd_help(message: Message, state: FSMContext):
+async def cmd_help(message: Message, state: FSMContext, bot: Bot):
     await message.delete()
-    await message.answer(
-        text=f"This bot was created by me.\n\nHere are my 🔗 links:",
+    await bot.send_message(
+        chat_id=message.from_user.id,
+        text=f"This bot was created by me 🙋‍♂️\n\nThere are my 🔗 links:",
         reply_markup=links_kb
     )
 
@@ -72,7 +75,7 @@ async def cmd_help(message: Message, state: FSMContext):
 @router.message(StateFilter(None), Command("guess"))
 @router.message(StateFilter("GuessRandom:finish_or_next"), Command("next"))
 @router.message(StateFilter("GuessRandom:finish_or_next"), F.text.lower() == "next")
-async def cmd_guess_no_state(message: Message, state: FSMContext):
+async def cmd_guess_no_state(message: Message, state: FSMContext, bot: Bot):
     await message.delete()
     global random_sentence
     # random_sentence = choice(tuple(sentences.keys()))
@@ -81,7 +84,8 @@ async def cmd_guess_no_state(message: Message, state: FSMContext):
     data['answer'] = random_sentence.get('tense')
     data['sentence'] = random_sentence.get('sentence')
     await state.set_data(data)
-    await message.answer(
+    await bot.send_message(
+            chat_id=message.from_user.id,
             text=f"Guess the tense\n\n💬 <b>{random_sentence['sentence']}</b>",
             reply_markup=tenses_kb()
     )
@@ -120,26 +124,29 @@ async def cmd_finish_guess_random_state(message: Message, state: FSMContext):
 
 @router.message(StateFilter("GuessRandom:finish_or_next"), Command("finish"))
 @router.message(StateFilter("GuessRandom:finish_or_next"), F.text.lower() == "finish")
-async def cmd_finish_finish_or_next_state(message: Message, state: FSMContext):
+async def cmd_finish_finish_or_next_state(message: Message, state: FSMContext, bot: Bot):
     await message.delete()
     await state.clear()
-    await message.answer(
+    await bot.send_message(
+        chat_id=message.from_user.id,
         text="The game is over.\nSee you later 🖐\n\nOr use the /guess command to start the game again.", # должно быть сообщение о счете: Your Score is 18/20 or 90% correct answers.
         reply_markup=ReplyKeyboardRemove()
     )
 
 
 @router.message(GuessRandom.guess_random, F.text.in_(available_tenses))
-async def tense_chosen(message: Message, state: FSMContext):
+async def tense_chosen(message: Message, state: FSMContext, bot: Bot):
     await message.delete()
     await state.update_data(chosen_tense=message.text.lower())
     if message.text == random_sentence['tense']:
-        await message.answer(
+        await bot.send_message(
+            chat_id=message.from_user.id,
             text=f"✅ <b>{available_tenses[message.text]}</b>\nAnd it`s correct!",
             reply_markup=finish_next_kb()
         )
     else:
-        await message.answer(
+        await bot.send_message(
+            chat_id=message.from_user.id,
             text=f"🔴 <b>{available_tenses[message.text]}</b>\nRight answer is <b>{available_tenses[random_sentence['tense']]}</b>",
             reply_markup=finish_next_kb()
         )
