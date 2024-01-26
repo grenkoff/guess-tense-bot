ab07855ab92f218c79b0ab219149c6a77524fa5b
Alexander Grenkov <a.grenkov@gmail.com>
2024-01-26 09:38:36 +0600

    replace inline keyboards instead reply keyboards

--- bot/handlers/common.py
+++ bot/handlers/common.py
@@ -1,17 +1,11 @@
-from random import choice
-
-from aiogram import F, Router, Bot
+from aiogram import F, Router, Bot, html
 from aiogram.filters import CommandStart, Command, StateFilter
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import default_state
-from aiogram.types import Message, ReplyKeyboardRemove
-from aiogram.methods.delete_message import DeleteMessage
+from aiogram.types import Message, CallbackQuery, ReplyKeyboardRemove
 
-# from bot_instance import bot
 from utils.states import GuessRandom
 from data.data_fetcher import get_random
-from keyboards.reply import tenses_kb,finish_next_kb
-from keyboards.inline import finish_or_next_kb, links_kb
+from keyboards.inline import tenses_inline_kb, finish_or_next_kb, links_kb
 from welcome import WELCOME, HELP
 
 
@@ -34,126 +28,128 @@ available_tenses = {
 
 
 @router.message(StateFilter(None), CommandStart())
-async def cmd_start_no_state(message: Message, state: FSMContext, bot: Bot):
+async def cmd_start_no_state(message: Message, bot: Bot):
     await message.delete()
     await bot.send_message(
         chat_id=message.from_user.id,
         text=f"Hi, <b>{message.from_user.first_name}</b>! 🤝\n{WELCOME}",
-        reply_markup=ReplyKeyboardRemove()
+        reply_markup=ReplyKeyboardRemove(),
     )
 
 
-@router.message(StateFilter("GuessRandom:guess_random"), CommandStart())
-async def cmd_start_guess_random_state(message: Message, state: FSMContext):
+@router.message(StateFilter(None), Command("help"))
+async def cmd_help(message: Message, bot: Bot):
     await message.delete()
+    await bot.send_message(chat_id=message.from_user.id, text=f"{HELP}")
 
 
-@router.message(StateFilter("GuessRandom:finish_or_next"), CommandStart())
-async def cmd_start_finish_or_next_state(message: Message, state: FSMContext):
-    await message.delete()
-
-
-@router.message(Command("help"))
-async def cmd_help(message: Message, state: FSMContext, bot: Bot):
-    await message.delete()
-    await bot.send_message(
-        chat_id=message.from_user.id,
-        text=f"{HELP}"
-    )
-
-
-@router.message(Command("author"))
-async def cmd_help(message: Message, state: FSMContext, bot: Bot):
+@router.message(StateFilter(None), Command("author"))
+async def cmd_author(message: Message, bot: Bot):
     await message.delete()
     await bot.send_message(
         chat_id=message.from_user.id,
         text=f"This bot was created by me 🙋‍♂️\n\nThere are my 🔗 links:",
-        reply_markup=links_kb
+        reply_markup=links_kb,
     )
 
 
 @router.message(StateFilter(None), Command("guess"))
-@router.message(StateFilter("GuessRandom:finish_or_next"), Command("next"))
-@router.message(StateFilter("GuessRandom:finish_or_next"), F.text.lower() == "next")
 async def cmd_guess_no_state(message: Message, state: FSMContext, bot: Bot):
     await message.delete()
     global random_sentence
-    # random_sentence = choice(tuple(sentences.keys()))
     random_sentence = await get_random()
     data = await state.get_data()
-    data['answer'] = random_sentence.get('tense')
-    data['sentence'] = random_sentence.get('sentence')
+    data["answer"] = random_sentence.get("tense")
+    data["sentence"] = random_sentence.get("sentence")
     await state.set_data(data)
     await bot.send_message(
-            chat_id=message.from_user.id,
-            text=f"Guess the tense\n\n💬 <b>{random_sentence['sentence']}</b>",
-            reply_markup=tenses_kb()
+        chat_id=message.from_user.id,
+        text="Guess the tense 👇",
+    )
+    await bot.send_message(
+        chat_id=message.from_user.id,
+        text=f"💬 <b>{html.quote(random_sentence['sentence'])}</b>",
+        reply_markup=tenses_inline_kb.as_markup(),
     )
     await state.set_state(GuessRandom.guess_random)
 
 
 @router.message(StateFilter("GuessRandom.guess_random"), Command("guess"))
-async def cmd_guess_guess_random_state(message: Message, state: FSMContext):
+async def cmd_guess_guess_random_state(message: Message):
     await message.delete()
 
 
 @router.message(StateFilter("GuessRandom.finish_or_next"), Command("guess"))
-async def cmd_guess_finish_or_next_state(message: Message, state: FSMContext):
+async def cmd_guess_finish_or_next_state(message: Message):
     await message.delete()
-
-
-@router.message(StateFilter(None), Command("next"))
-async def cmd_next_no_state(message: Message, state: FSMContext):
-    await message.delete()
-
-
-@router.message(StateFilter("GuessRandom:guess_random"), Command("next"))
-async def cmd_next_guess_random_state(message: Message, state: FSMContext):
-    await message.delete()
-
-
-@router.message(StateFilter(None), Command("finish"))
-async def cmd_finish_no_state(message: Message, state: FSMContext):
-    await message.delete()
-
-
-@router.message(StateFilter("GuessRandom:guess_random"), Command("finish"))
-async def cmd_finish_guess_random_state(message: Message, state: FSMContext):
-    await message.delete()
-
-
-@router.message(StateFilter("GuessRandom:finish_or_next"), Command("finish"))
-@router.message(StateFilter("GuessRandom:finish_or_next"), F.text.lower() == "finish")
-async def cmd_finish_finish_or_next_state(message: Message, state: FSMContext, bot: Bot):
-    await message.delete()
-    await state.clear()
-    await bot.send_message(
-        chat_id=message.from_user.id,
-        text="The game is over.\nSee you later 🖐\n\nOr use the /guess command to start the game again.", # должно быть сообщение о счете: Your Score is 18/20 or 90% correct answers.
-        reply_markup=ReplyKeyboardRemove()
-    )
-
-
-@router.message(GuessRandom.guess_random, F.text.in_(available_tenses))
-async def tense_chosen(message: Message, state: FSMContext, bot: Bot):
-    await message.delete()
-    await state.update_data(chosen_tense=message.text.lower())
-    if message.text == random_sentence['tense']:
-        await bot.send_message(
-            chat_id=message.from_user.id,
-            text=f"✅ <b>{available_tenses[message.text]}</b>\nAnd it`s correct!",
-            reply_markup=finish_next_kb()
-        )
-    else:
-        await bot.send_message(
-            chat_id=message.from_user.id,
-            text=f"🔴 <b>{available_tenses[message.text]}</b>\nRight answer is <b>{available_tenses[random_sentence['tense']]}</b>",
-            reply_markup=finish_next_kb()
-        )
-
-    await state.set_state(GuessRandom.finish_or_next)
 
 
 @router.message()
 async def delete_all_messages(message: Message):
     await message.delete()
+
+
+@router.callback_query(F.data.in_(available_tenses))
+async def tense_chosen(callback: CallbackQuery, state: FSMContext, bot: Bot):
+    await state.update_data(chosen_tense=callback.data.lower())
+    await bot.edit_message_reply_markup(
+        chat_id=callback.from_user.id,
+        message_id=callback.message.message_id,
+        reply_markup=None,
+    )
+    if callback.data == random_sentence["tense"]:
+        await bot.edit_message_text(
+            text=f"💬 <b>{html.quote(random_sentence['sentence'])}</b>\n\n✅ <b>{available_tenses[callback.data]}</b>\nAnd it`s correct!",
+            chat_id=callback.from_user.id,
+            message_id=callback.message.message_id,
+            reply_markup=finish_or_next_kb.as_markup(),
+        )
+    else:
+        await bot.edit_message_text(
+            text=f"💬 <b>{html.quote(random_sentence['sentence'])}</b>\n\n🔴 <b>{available_tenses[callback.data]}</b>\nRight answer is <b>{available_tenses[random_sentence['tense']]}</b>",
+            chat_id=callback.from_user.id,
+            message_id=callback.message.message_id,
+            reply_markup=finish_or_next_kb.as_markup(),
+        )
+
+    await state.set_state(GuessRandom.finish_or_next)
+    await callback.answer()
+
+
+@router.callback_query(F.data == "finish")
+async def cmd_finish_finish_or_next_state(
+    callback: CallbackQuery, state: FSMContext, bot: Bot
+):
+    await state.clear()
+    await bot.edit_message_reply_markup(
+        chat_id=callback.from_user.id,
+        message_id=callback.message.message_id,
+        reply_markup=None,
+    )
+    await callback.message.answer(
+        text="The game is over 🏁\nSee you later 🖐\n\nOr use the /guess command to start the game again. ", # должно быть сообщение о счете: Your Score is 18/20 or 90% correct answers.
+        reply_markup=ReplyKeyboardRemove(),
+    )
+    await callback.answer()
+
+
+@router.callback_query(F.data == "next")
+async def cmd_next_finish_or_next_state(
+    callback: CallbackQuery, state: FSMContext, bot: Bot
+):
+    await bot.edit_message_reply_markup(
+        chat_id=callback.from_user.id,
+        message_id=callback.message.message_id,
+        reply_markup=None,
+    )
+    global random_sentence
+    random_sentence = await get_random()
+    data = await state.get_data()
+    data["answer"] = random_sentence.get("tense")
+    data["sentence"] = random_sentence.get("sentence")
+    await state.set_data(data)
+    await callback.message.answer(
+        text=f"💬 <b>{html.quote(random_sentence['sentence'])}</b>",
+        reply_markup=tenses_inline_kb.as_markup(),
+    )
+    await state.set_state(GuessRandom.guess_random)
--- bot/keyboards/inline.py
+++ bot/keyboards/inline.py
@@ -1,4 +1,5 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 links_kb = InlineKeyboardMarkup(
@@ -6,15 +7,51 @@ links_kb = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(text="Telegram", url="tg://resolve?domain=agrenkoff"),
             InlineKeyboardButton(text="Github", url="https://github.com/grenkoff/guess-tense-bot")
+        ],
+        [
+            InlineKeyboardButton(
+                text="Buy me a coffee ☕",
+                url="https://www.buymeacoffee.com/grenkoff"
+            )
         ]
     ]
 )
 
-finish_or_next_kb = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [
-            InlineKeyboardButton(text="Finish", url="tg://resolve?domain=agrenkoff"),
-            InlineKeyboardButton(text="Next", url="https://github.com/grenkoff/guess-tense-bot")
-        ]
-    ]
+# finish_or_next_kb = InlineKeyboardMarkup(
+#     inline_keyboard=[
+#         [
+#             InlineKeyboardButton(text="Finish", url="tg://resolve?domain=agrenkoff"),
+#             InlineKeyboardButton(text="Next", url="https://github.com/grenkoff/guess-tense-bot")
+#         ]
+#     ]
+# )
+
+
+finish_or_next_kb = InlineKeyboardBuilder()
+finish_b = InlineKeyboardButton(
+    text="Finish",
+    callback_data="finish"
 )
+next_b = InlineKeyboardButton(
+    text="Next",
+    callback_data="next"
+)
+finish_or_next_kb.row(finish_b, next_b)
+
+
+tenses_inline_kb = InlineKeyboardBuilder()
+pr_si = InlineKeyboardButton(text="Pr Si", callback_data="Pr Si")
+pr_co = InlineKeyboardButton(text="Pr Co", callback_data="Pr Co")
+pr_pe = InlineKeyboardButton(text="Pr Pe", callback_data="Pr Pe")
+pr_pe_co = InlineKeyboardButton(text="Pr Pe Co", callback_data="Pr Pe Co")
+pa_si = InlineKeyboardButton(text="Pa Si", callback_data="Pa Si")
+pa_co = InlineKeyboardButton(text="Pa Co", callback_data="Pa Co")
+pa_pe = InlineKeyboardButton(text="Pa Pe", callback_data="Pa Pe")
+pa_pe_co = InlineKeyboardButton(text="Pa Pe Co", callback_data="Pa Pe Co")
+fu_si = InlineKeyboardButton(text="Fu Si", callback_data="Fu Si")
+fu_co = InlineKeyboardButton(text="Fu Co", callback_data="Fu Co")
+fu_pe = InlineKeyboardButton(text="Fu Pe", callback_data="Fu Pe")
+fu_pe_co = InlineKeyboardButton(text="Fu Pe Co", callback_data="Fu Pe Co")
+tenses_inline_kb.row(pr_si, pr_co, pr_pe, pr_pe_co)\
+                .row(pa_si, pa_co, pa_pe, pa_pe_co)\
+                .row(fu_si, fu_co, fu_pe, fu_pe_co)
