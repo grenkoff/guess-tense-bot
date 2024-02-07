07d54a6fffd6e33b23446b6ca6cb94da84409a17
Alexander Grenkov <a.grenkov@gmail.com>
2024-02-07 13:04:56 +0600

    replace Legend instead Legends

--- bot/common/bot_cmds_list.py
+++ bot/common/bot_cmds_list.py
@@ -3,7 +3,7 @@ from aiogram.types import BotCommand
 
 private = [
     BotCommand(command="start", description="Welcome message"),
-    BotCommand(command="help", description="Legends"),
+    BotCommand(command="help", description="Legend"),
     BotCommand(command="guess", description="Start new game"),
     BotCommand(command="author", description="Credits"),
 ]
